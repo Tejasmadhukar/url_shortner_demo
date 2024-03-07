c9ce62d4f938d6f6acb6fbf28dea6a4572ce1531
Tejas013 <tejasmadhukar6@gmail.com>
2024-03-08 00:18:41 +0530

    changed colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import { api } from "~/trpc/server";
 export default async function Home() {
   const session = await getServerAuthSession();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-slate-500 to-slate-900 text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          URL <span className="text-[hsl(280,100%,70%)]">Shortner</span> Demo
+          URL <span className="text-stone-200">Shortner</span> Demo
         </h1>
         <div className="flex flex-col items-center gap-2">
           <div className="flex flex-col items-center justify-center gap-4">
