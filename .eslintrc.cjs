36045f6ebb2392e8040496d3533385f5f06a79a7
Tejas013 <tejasmadhukar6@gmail.com>
2024-03-13 15:55:18 +0530

    Added stricter rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ const config = {
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "unused-imports"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
@@ -24,7 +24,7 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
@@ -32,6 +32,7 @@ const config = {
         checksVoidReturn: { attributes: false },
       },
     ],
+    "unused-imports/no-unused-imports": "error",
   },
 };
 
