b2f7088b95668081f69de42d1786729381d5521f
Tejas013 <tejasmadhukar6@gmail.com>
2024-03-13 16:41:35 +0530

    Added notification on click and time range validation on click

--- src/app/[slug]/route.ts
+++ src/app/[slug]/route.ts
@@ -2,6 +2,7 @@ import { permanentRedirect, redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 import { getServerAuthSession } from "~/server/auth";
 import { db } from "~/server/db";
+import { NotifyOnClick } from "./send_mail";
 
 export async function GET(request: Request) {
   const urlParts = request.url.split("/");
@@ -25,10 +26,22 @@ export async function GET(request: Request) {
     );
   }
 
+  if (!(new Date() < url.endTime && new Date() >= url.startTime)) {
+    return NextResponse.json(
+      {
+        message: `This url started at ${url.startTime.toLocaleString()} and ends at ${url.endTime.toLocaleString()}. If you're seeing this message then it is not in valid time range`,
+      },
+      { status: 400 },
+    );
+  }
+
   if (url.isAuthRequired) {
     const session = await getServerAuthSession();
-    if (!session) {
+    if (!session?.user.email) {
       return redirect("/api/auth/signin");
+    }
+    if (url.isNotificationRequired) {
+      await NotifyOnClick(url.userId, session.user.email, new Date());
     }
   }
 
