ee2b7f2d467af3fe3486cdeaa05e299b5accf731
Tejas013 <tejasmadhukar6@gmail.com>
2024-03-23 20:04:07 +0530

    Made basic page to fetch and redirect

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,29 @@
+import { permanentRedirect } from "next/navigation";
+import { db } from "~/server/db";
+
+export default async function Redirect({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const tinyurl = params.slug;
+
+  const url = await db.query.urls.findFirst({
+    where: (tiny, { eq }) => eq(tiny.tinyurl, tinyurl),
+  });
+
+  if (!url) return <h1>No Url found</h1>;
+
+  if (!(new Date() < url.endTime && new Date() >= url.startTime))
+    return (
+      <h1>
+        This url started at {url.startTime.toLocaleString()} and ends at
+        {url.endTime.toLocaleString()}. If you are seeing this message then url
+        has not started yet or has expired.
+      </h1>
+    );
+
+  if (!url.isAuthRequired) return permanentRedirect(url.forwardedTo);
+
+  return <h1>auth and notification UI condition</h1>;
+}
